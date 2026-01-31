cb493e1e23c209ebc58995b13fb8b002cfe5a9c8
rdbo <rdbodev@gmail.com>
2026-01-31 00:31:19 +0000

    added partial book name match and error handling

--- bin/discord-bible-bot.rb
+++ bin/discord-bible-bot.rb
@@ -16,7 +16,21 @@ def citation(book_name, chapter_num, versicle_start, versicle_end=nil)
     versicle_start < 1 or
     (versicle_end and versicle_end < 1 || versicle_end < versicle_start)
 
-  book = $bible.books.find{|x| x.name == book_name} || raise("Book not found: #{book_name}")
+  # Find exact book match
+  book_name.downcase! # All the searches use lowercase
+  book = $bible.books.find{|x| x.name.downcase == book_name}
+
+  # Find partial book match
+  if not book
+    book = $bible.books.find{ |x|
+      cur_name = x.name.downcase
+      cur_name.start_with?(book_name) ||
+        cur_name.gsub(/\s/, "").start_with?(book_name)
+    }
+  end
+
+  raise "Book not found: #{book_name}" if not book
+
   chapter = book.chapters[chapter_num - 1]
   raise "Invalid chapter" if not chapter
 
@@ -47,10 +61,20 @@ def random_citation()
   citation(book.name, chapter_index + 1, versicle_start, versicle_end)
 end
 
-bot.register_application_command(:books, 'Get a list of the books in the Bible')
-bot.application_command(:books) do |event|
-  event.respond(content: books(), ephemeral: true)
+def command_wrapper(&block)
+  proc \
+    do |event, *args, **kwargs|
+      block.call(event, *args, **kwargs)
+    rescue => e
+      puts "ERROR: #{e}"
+      event.respond(content: "Something bad happened :(", ephemeral: true)
+    end
 end
+
+bot.register_application_command(:books, 'Get a list of the books in the Bible')
+bot.application_command(:books, &command_wrapper do |event|
+  event.respond(content: books(), ephemeral: true)
+end)
 
 bot.register_application_command(:citation, 'Get a specific citation from the Bible') do |cmd|
   cmd.string('book', 'The book to get the citation from', required: true)
@@ -58,18 +82,18 @@ bot.register_application_command(:citation, 'Get a specific citation from the Bi
   cmd.integer('versicle_start', 'The first versicle of the citation', required: true)
   cmd.integer('versicle_end', 'The last versicle of the citation', required: false)
 end
-bot.application_command(:citation) do |event|
+bot.application_command(:citation, &command_wrapper do |event|
   book = event.options['book']
   chapter = event.options['chapter']
   versicle_start = event.options['versicle_start']
   versicle_end = event.options['versicle_end']
   event.respond(content: citation(book, chapter, versicle_start, versicle_end))
-end
+end)
 
 bot.register_application_command(:random_citation, 'Get a random citation from the Bible')
-bot.application_command(:random_citation) do |event|
+bot.application_command(:random_citation, &command_wrapper do |event|
   citation = random_citation()
   event.respond(content: "#{citation}")
-end
+end)
 
 bot.run
