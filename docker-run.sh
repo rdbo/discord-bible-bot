e1f9b423d6fb9c99687f83d150391950b211ac55
rdbo <rdbodev@gmail.com>
2026-02-02 23:11:43 +0000

    fix docker file copy

--- docker-run.sh
+++ docker-run.sh
@@ -4,4 +4,4 @@ set -e
 
 mkdir -p cache
 docker build -t discord-bible-bot .
-docker run -v "$(pwd)/cache:/app/cache" --restart on-failure:5 discord-bible-bot
+docker run -v "$(pwd)/cache:/app/cache" -v "$(pwd)/.env:/app/.env" --restart on-failure:5 discord-bible-bot
