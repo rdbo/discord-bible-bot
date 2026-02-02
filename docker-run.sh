be9dcfa7dfa862d0faff821b0ae3fc8987b3970d
rdbo <rdbodev@gmail.com>
2026-02-02 23:06:25 +0000

    fix docker mount

--- docker-run.sh
+++ docker-run.sh
@@ -4,4 +4,4 @@ set -e
 
 mkdir -p cache
 docker build -t discord-bible-bot .
-docker run -v cache:/app/cache --restart on-failure:5 discord-bible-bot
+docker run -v "$(pwd)/cache:/app/cache" --restart on-failure:5 discord-bible-bot
